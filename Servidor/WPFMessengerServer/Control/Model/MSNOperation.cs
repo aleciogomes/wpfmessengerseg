911b05b133e4041af7d4cdeb0c53498b1cb8e2d2
areshio <areshio@3721dff4-e601-e5b6-43e0-8d9034bc20b4>
2010-07-17 21:16:41 +0000

    Classes para controlar Operação e Recurso.

---
+++ Servidor/WPFMessengerServer/Control/Model/MSNOperation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WPFMessengerServer.Control.Model
+{
+    class MSNOperation
+    {
+        public int Codigo { get; set; }
+        public String Descricao { get; set; }
+        public MSNResource Resource { get; set; }
+    }
+}
---
+++ Servidor/WPFMessengerServer/Control/Model/MSNResource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WPFMessengerServer.Control.Model
+{
+    class MSNResource
+    {
+        public int Codigo { get; set; }
+        public String Descricao { get; set; }
+    }
+}
